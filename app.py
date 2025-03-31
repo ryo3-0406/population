3c92545bb5e61135779bb130599549472417d459
ryosuke <rogerrafarn0324@gmail.com>
2025-03-31 13:30:59 +0900

    .

--- app.py
+++ app.py
@@ -22,9 +22,10 @@ st.set_page_config(page_title="令和2年国勢調査 人口集計", layout="wid
 # メインタイトル
 st.title("人口/世帯数 基本集計")
 image = Image.open("./fig/title.jpg")
+filesize = 400
 ratio = image.height / image.width
-new_height = int(500 * ratio)
-resized_image = image.resize((500, new_height), Image.LANCZOS)
+new_height = int(filesize * ratio)
+resized_image = image.resize((filesize, new_height), Image.LANCZOS)
 
 st.image(resized_image)
 # st.image(image, use_container_width=True)
