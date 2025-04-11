60d7c9bdf899bb76d4dfc89783871b838bac4bcc
ryosuke <rogerrafarn0324@gmail.com>
2025-04-11 22:21:26 +0900

    .

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
     # new_height = int(filesize * ratio)
     # resized_image = image.resize((filesize, new_height), Image.LANCZOS)
     # st.image(resized_image)
-    st.title("国勢調査2020")
+    st.title("令和2年国勢調査")
     st.subheader("人口＆世帯数 基本集計")
     st.sidebar.header("集計設定")
 
